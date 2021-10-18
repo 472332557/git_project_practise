8e8e6517fc9ebbff63360f953414aa10348ca4ac
liangzc <472332557@qq.com>
2021-10-18 13:48:18 +0800

    rabbitmq练习+1

--- src/main/java/com/liangzc/example/mq/rabbitmq/simple/ProduceTest.java
+++ src/main/java/com/liangzc/example/mq/rabbitmq/simple/ProduceTest.java
@@ -11,36 +11,92 @@ public class ProduceTest {
 
     private static final String EXCHANGE_DIRECT = "exchange-direct";
     private static final String EXCHANGE_TOPIC = "exchange-topic";
-    private static final String EXCHANGE_FANOUT = "exchange-fanout";
+    private static final String EXCHANGE_FANOUT = "fanout-exechange";
     public static void main(String[] args) throws Exception {
+
+//        directTest();
+        toppicTest();
+
+    }
+
+
+    public static void fanoutTest(){
         Connection connection = null;
         Channel channel = null;
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setUsername("admin");
-            factory.setPassword("admin");
-            factory.setHost("119.23.189.136");
-            factory.setPort(5672);
-            factory.setVirtualHost("/");
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        factory.setHost("121.37.249.94");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+        try {
             connection = factory.newConnection();
-
-
             //创建一个channel
             channel = connection.createChannel();
-
             // 发送消息
             String msg = "GG,没中签！！！";
-
             // String exchange, String routingKey, BasicProperties props, byte[] body
 //            channel.basicPublish(EXCHANGE_DIRECT, "", null, msg.getBytes());
-        channel.basicPublish(EXCHANGE_FANOUT, "", null, msg.getBytes());
-
+            channel.basicPublish(EXCHANGE_FANOUT, "", null, msg.getBytes());
             System.out.println("消息已发送："+msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                channel.close();
+                connection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    public static void directTest() throws IOException, TimeoutException {
+        Connection connection = null;
+        Channel channel = null;
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        factory.setHost("121.37.249.94");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+            connection = factory.newConnection();
+            //创建一个channel
+            channel = connection.createChannel();
+            // 发送消息
+            String msg = "GG,没中签！！！";
+            // String exchange, String routingKey, BasicProperties props, byte[] body
+            channel.basicPublish(EXCHANGE_DIRECT, "aaa", null, msg.getBytes());
+//            channel.basicPublish(EXCHANGE_FANOUT, "", null, msg.getBytes());
+            System.out.println("消息已发送："+msg);
             channel.close();
             connection.close();
+    }
 
-            //看下改变使用git
+    public static void toppicTest() throws IOException, TimeoutException {
+        Connection connection = null;
+        Channel channel = null;
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        factory.setHost("121.37.249.94");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+        connection = factory.newConnection();
+        //创建一个channel
+        channel = connection.createChannel();
+        // 发送消息
+        String msg = "GG,没中签！！！";
+        // String exchange, String routingKey, BasicProperties props, byte[] body
+        channel.basicPublish(EXCHANGE_TOPIC, "com.11.22", null, msg.getBytes());
+//            channel.basicPublish(EXCHANGE_FANOUT, "", null, msg.getBytes());
+        System.out.println("消息已发送："+msg);
+        channel.close();
+        connection.close();
 
-            //再次使用git
     }
 }
