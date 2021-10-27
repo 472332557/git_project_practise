add2a392218e294511825c19306ed8ea5f3bd78c
liangzc <472332557@qq.com>
2021-10-27 18:39:12 +0800

    rabbitmq可靠性投递，生产者发送消息到服务器（broker），服务器做出响应，事物和confirm机制

---
+++ src/main/java/com/liangzc/example/mq/rabbitmq/reliable/txselect/ConsumerTest.java
@@ -0,0 +1,163 @@
+package com.liangzc.example.mq.rabbitmq.reliable.txselect;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ConsumerTest {
+
+    private static final String EXCHANGE_DIRECT = "exchange-direct";
+    private static final String EXCHANGE_TOPIC = "exchange-topic";
+    private static final String EXCHANGE_FANOUT = "fanout-exechange";
+    private static final String QUEUE_NAME_1 = "queue-1";
+    private static final String QUEUE_NAME_2 = "queue-2";
+    private static final String QUEUE_NAME_3 = "queue-3";
+    private static final String QUEUE_NAME_4 = "queue-4";
+    private static final String QUEUE_NAME_5 = "queue-5";
+    private static final String QUEUE_NAME_6 = "queue-6";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+//        directTest();
+//        topicTest();
+        fanoutTest();
+    }
+
+    public static void fanoutTest() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("121.37.249.94");
+        factory.setPort(5672);
+        //指定虚拟机
+        factory.setVirtualHost("/");
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+
+        //建立连接
+        Connection connection = factory.newConnection();
+        //创建消息信道
+        Channel channel = connection.createChannel();
+
+        //声明交换机:直连类型
+//        channel.exchangeDeclare(EXCHANGE_DIRECT, "direct", true, false, null);
+        //声明交换机:广播类型
+        channel.exchangeDeclare(EXCHANGE_FANOUT, "fanout", true, false, null);
+
+        //声明队列
+        channel.queueDeclare(QUEUE_NAME_1, true, false, false, null);
+        channel.queueDeclare(QUEUE_NAME_2, true, false, false, null);
+        System.out.println("waiting for message ..........");
+
+        //绑定交换机和队列
+//        channel.queueBind(QUEUE_NAME_1, EXCHANGE_TOPIC, "nai_xue");
+        channel.queueBind(QUEUE_NAME_1, EXCHANGE_FANOUT, "");
+        channel.queueBind(QUEUE_NAME_2, EXCHANGE_FANOUT, "");
+
+        //创建消费者
+        Consumer consumer = new DefaultConsumer(channel){
+
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String msg = new String(body,"UTF-8");
+                System.out.println("Received message :" + msg);
+                System.out.println("consumerTag :" + consumerTag);
+                System.out.println("deliveryTag :" +envelope.getDeliveryTag());
+            }
+        };
+        channel.basicConsume(QUEUE_NAME_1, true, consumer);
+        channel.basicConsume(QUEUE_NAME_2, true, consumer);
+    }
+
+    public static void directTest() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("121.37.249.94");
+        factory.setPort(5672);
+        //指定虚拟机
+        factory.setVirtualHost("/");
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+
+        //建立连接
+        Connection connection = factory.newConnection();
+        //创建消息信道
+        Channel channel = connection.createChannel();
+
+        //声明交换机:直连类型
+        channel.exchangeDeclare(EXCHANGE_DIRECT, "direct", true, false, null);
+        //声明交换机:广播类型
+//        channel.exchangeDeclare(EXCHANGE_FANOUT, "fanout", true, false, null);
+
+        //声明队列
+        channel.queueDeclare(QUEUE_NAME_3, true, false, false, null);
+        channel.queueDeclare(QUEUE_NAME_4, true, false, false, null);
+        System.out.println("waiting for message ..........");
+
+        //绑定交换机和队列
+//        channel.queueBind(QUEUE_NAME_1, EXCHANGE_TOPIC, "nai_xue");
+        channel.queueBind(QUEUE_NAME_3, EXCHANGE_DIRECT, "aaa");
+        channel.queueBind(QUEUE_NAME_4, EXCHANGE_DIRECT, "");
+
+        //创建消费者
+        Consumer consumer = new DefaultConsumer(channel){
+
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String msg = new String(body,"UTF-8");
+                System.out.println("Received message :" + msg);
+                System.out.println("consumerTag :" + consumerTag);
+                System.out.println("deliveryTag :" +envelope.getDeliveryTag());
+            }
+        };
+        channel.basicConsume(QUEUE_NAME_3, true, consumer);
+        channel.basicConsume(QUEUE_NAME_4, true, consumer);
+    }
+
+    public static void topicTest() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("121.37.249.94");
+        factory.setPort(5672);
+        //指定虚拟机
+        factory.setVirtualHost("/");
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+
+        //建立连接
+        Connection connection = factory.newConnection();
+        //创建消息信道
+        Channel channel = connection.createChannel();
+
+        //声明交换机:直连类型
+        channel.exchangeDeclare(EXCHANGE_TOPIC, "topic", true, false, null);
+        //声明交换机:广播类型
+//        channel.exchangeDeclare(EXCHANGE_FANOUT, "fanout", true, false, null);
+
+        //声明队列
+        channel.queueDeclare(QUEUE_NAME_5, true, false, false, null);
+        channel.queueDeclare(QUEUE_NAME_6, true, false, false, null);
+        System.out.println("waiting for message ..........");
+
+        //绑定交换机和队列
+//        channel.queueBind(QUEUE_NAME_1, EXCHANGE_TOPIC, "nai_xue");
+        channel.queueBind(QUEUE_NAME_5, EXCHANGE_TOPIC, "com.#");
+        channel.queueBind(QUEUE_NAME_6, EXCHANGE_TOPIC, "com.*");
+
+        //创建消费者
+        Consumer consumer = new DefaultConsumer(channel){
+
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String msg = new String(body,"UTF-8");
+                System.out.println("Received message :" + msg);
+                System.out.println("consumerTag :" + consumerTag);
+                System.out.println("deliveryTag :" +envelope.getDeliveryTag());
+                System.out.println("交换机是："+envelope.getExchange());
+                System.out.println("消费的路由键是："+envelope.getRoutingKey());
+                System.out.println("消费内容类型："+properties.getContentType());
+                System.out.println("=============================================================================");
+            }
+        };
+        channel.basicConsume(QUEUE_NAME_5, true, consumer);
+        channel.basicConsume(QUEUE_NAME_6, true, consumer);
+    }
+
+}
---
+++ src/main/java/com/liangzc/example/mq/rabbitmq/reliable/txselect/ProduceTest.java
@@ -0,0 +1,134 @@
+package com.liangzc.example.mq.rabbitmq.reliable.txselect;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmListener;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ProduceTest {
+
+    private static final String EXCHANGE_DIRECT = "exchange-direct";
+    private static final String EXCHANGE_TOPIC = "exchange-topic";
+    private static final String EXCHANGE_FANOUT = "fanout-exechange";
+    public static void main(String[] args) throws Exception {
+
+//        directTest();
+//        toppicTest();
+        fanoutTest();
+    }
+
+
+    /**
+     * channel.txSelect();开启事物机制
+     */
+    public static void fanoutTest(){
+        Connection connection = null;
+        Channel channel = null;
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        factory.setHost("121.37.249.94");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+        try {
+            connection = factory.newConnection();
+            //创建一个channel
+            channel = connection.createChannel();
+            // 发送消息
+            String msg = "GG,没中签！！！";
+            //开启事物确认，生产者发送消息给broker，服务器会有一个响应
+            channel.txSelect();
+            // String exchange, String routingKey, BasicProperties props, byte[] body
+//            channel.basicPublish(EXCHANGE_DIRECT, "", null, msg.getBytes());
+            channel.basicPublish(EXCHANGE_FANOUT, "", null, msg.getBytes());
+            Integer num = 9 / 0;
+            channel.basicPublish(EXCHANGE_FANOUT, "", null,String.valueOf(num).getBytes());
+            //提交事物给服务器（broker）
+            channel.txCommit();
+            System.out.println("消息已发送："+msg);
+        } catch (Exception e) {
+            try {
+                //否则该消息被回滚
+                channel.txRollback();
+                System.out.println("消息被回滚了！！！");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        } finally {
+            try {
+                channel.close();
+                connection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * channel.confirmSelect(); confirm确认机制
+     * @throws IOException
+     * @throws TimeoutException
+     */
+    public static void directTest() throws IOException, TimeoutException {
+        Connection connection = null;
+        Channel channel = null;
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        factory.setHost("121.37.249.94");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+            connection = factory.newConnection();
+            //创建一个channel
+            channel = connection.createChannel();
+            channel.confirmSelect();
+            // 发送消息
+            String msg = "GG,没中签！！！";
+            // String exchange, String routingKey, BasicProperties props, byte[] body
+            channel.basicPublish(EXCHANGE_DIRECT, "aaa", null, msg.getBytes());
+//            channel.basicPublish(EXCHANGE_FANOUT, "", null, msg.getBytes());
+        channel.addConfirmListener(new ConfirmListener() {
+            @Override
+            public void handleAck(long deliveryTag, boolean multiple) throws IOException {
+
+            }
+
+            @Override
+            public void handleNack(long deliveryTag, boolean multiple) throws IOException {
+
+            }
+        });
+            System.out.println("消息已发送："+msg);
+            channel.close();
+            connection.close();
+    }
+
+    public static void toppicTest() throws IOException, TimeoutException {
+        Connection connection = null;
+        Channel channel = null;
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        factory.setHost("121.37.249.94");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+        connection = factory.newConnection();
+        //创建一个channel
+        channel = connection.createChannel();
+        // 发送消息
+        String msg = "GG,没中签！！！";
+        // String exchange, String routingKey, BasicProperties props, byte[] body
+        channel.basicPublish(EXCHANGE_TOPIC, "com.aa.bb", null, msg.getBytes());
+//            channel.basicPublish(EXCHANGE_FANOUT, "", null, msg.getBytes());
+        System.out.println("消息已发送："+msg);
+        channel.close();
+        connection.close();
+
+    }
+}
