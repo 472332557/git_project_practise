0a622939cfee7c7fc879333aedbcd7b8d1a49b3f
liangzc <472332557@qq.com>
2022-02-23 14:01:52 +0800

    读文件

--- src/main/java/com/liangzc/example/HashMapTest.java
+++ src/main/java/com/liangzc/example/HashMapTest.java
@@ -1,6 +1,8 @@
 package com.liangzc.example;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HashMapTest {
@@ -8,9 +10,25 @@ public class HashMapTest {
     public static void main(String[] args) {
 
 
-        Map<String, Object> map = new HashMap<>();
-        map.put("jiji", 123);
-        System.out.println(map);
+        String bankCode = " 1000 2003 0050 3156 2014 ";
+
+        System.out.println(bankCode);
+        System.out.println(bankCode.trim().replaceAll("\\s",""));
+
+        List<String> list = new ArrayList<>();
+        list.add("1");
+
+        System.out.println(Math.max(10, 1));
+
+        /**
+         *  1000  0100
+         *  0111
+         *
+         *  1100  0110
+         */
+        int a = 8 >> 1;
+        System.out.println(a);
+
 
     }
 }
--- src/main/java/com/liangzc/example/io/ReadTest.java
+++ src/main/java/com/liangzc/example/io/ReadTest.java
@@ -1,9 +1,60 @@
 package com.liangzc.example.io;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class ReadTest {
     public static void main(String[] args) throws IOException {
+
+        String basePath = "D:/twscerts/C00000000X77";
+
+        File file = new File(basePath);
+
+        System.out.println("是否是文件："+file.isFile());
+
+        System.out.println("是否存在："+file.exists());
+
+        System.out.println("是否是一个文件夹："+file.isDirectory());
+
+        String pfxPath = "";
+        String crtPath = "";
+        String iniPath = "";
+
+        for (File listFile : file.listFiles()) {
+            System.out.println(listFile.isFile());
+            System.out.println("存在否："+listFile.exists());
+            System.out.println("文件路径："+listFile.getPath());
+            System.out.println("文件名称："+listFile.getName());
+            if (listFile.getName().endsWith(".pfx")){
+                pfxPath = listFile.getPath();
+            }
+            if (listFile.getName().endsWith(".crt")){
+                crtPath = listFile.getPath();
+            }
+            if(listFile.getName().endsWith(".ini")){
+                iniPath = listFile.getPath();
+            }
+        }
+
+        System.out.println("pfxPath:"+pfxPath);
+        System.out.println("crtPath:"+crtPath);
+        System.out.println("iniPath:" + iniPath);
+
+        InputStream inputStream = new FileInputStream(iniPath);
+        int read =0;
+        StringBuilder sb = new StringBuilder();
+/*        while ((read = inputStream.read()) != -1){
+            sb.append((char) read);
+        }*/
+        byte[] bytes = new byte[1024];
+        while ((read = inputStream.read(bytes)) != -1){
+            sb.append(new String(bytes, 0, read));
+        }
+
+        System.out.println(sb);
+
 
 
     }
