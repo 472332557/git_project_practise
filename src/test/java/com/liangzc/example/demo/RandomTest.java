2bbc6dae17dbea5ee3e55c60c433704cddb44b6d
liangzc <472332557@qq.com>
2022-03-22 17:53:49 +0800

    random

---
+++ src/test/java/com/liangzc/example/demo/RandomTest.java
@@ -0,0 +1,14 @@
+package com.liangzc.example.demo;
+
+import java.util.Map;
+import java.util.Random;
+
+public class RandomTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(new Random().nextInt(100));
+        System.out.println(Math.round(Math.random() * 100));
+
+    }
+}
