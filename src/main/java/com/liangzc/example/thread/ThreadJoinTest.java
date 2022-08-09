d0fa4e816ac49e04245c49e42d2ffce5922a8625
liangzc <472332557@qq.com>
2022-08-09 10:28:46 +0800

    thread join复习

--- src/main/java/com/liangzc/example/thread/ThreadJoinTest.java
+++ src/main/java/com/liangzc/example/thread/ThreadJoinTest.java
@@ -25,7 +25,11 @@ public class ThreadJoinTest {
         });
 
         thread1.start();
-        //等待Thread1线程执行结束，如果没有执行结束，则阻塞main线程
+        /**
+         *  等待Thread1线程执行结束，如果没有执行结束，则阻塞main线程
+         *  thread.join本质是：将该线程加入到调用者线程当中，使得加入的线程的执行结果对调用者线程可见；
+         *  也就是：阻塞当前调用者线程，等待该加入的子线程执行结束，然后继续去执行调用的子线程。
+         */
         thread1.join();
         thread2.start();
 
