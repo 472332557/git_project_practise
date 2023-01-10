13e9275be6f7d684cbca7eb793522fe063d05f5a
liangzc <472332557@qq.com>
2023-01-10 21:00:07 +0800

    JUC Semaphore使用说明

--- src/main/java/com/liangzc/example/thread/juc/semaphore/SemaphoreMain.java
+++ src/main/java/com/liangzc/example/thread/juc/semaphore/SemaphoreMain.java
@@ -7,6 +7,10 @@ public class SemaphoreMain {
 
     public static void main(String[] args) {
 
+        /**
+         *      信号量，我更倾向于称之为令牌，每个线程先要去获取令牌：semaphore.acquire()，
+         *      才有执行权限，等处理完成释放令牌：semaphore.release()，其他线程再去获得令牌执行
+         */
 
         Semaphore semaphore = new Semaphore(1);
 
