3734c3dbe2a8304190ccbf209b07467c204aa77b
liangzc <472332557@qq.com>
2023-07-10 17:59:09 +0800

    concurrentHashMap 源码查看

--- src/main/java/com/liangzc/example/collection/MapTest.java
+++ src/main/java/com/liangzc/example/collection/MapTest.java
@@ -2,6 +2,8 @@ package com.liangzc.example.collection;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class MapTest {
 
@@ -9,6 +11,10 @@ public class MapTest {
 
         Map map = new HashMap();
         map.put(1, 1);
+
+        ConcurrentMap concurrentMap = new ConcurrentHashMap();
+        concurrentMap.put("1", 1);
+
 
     }
 }
