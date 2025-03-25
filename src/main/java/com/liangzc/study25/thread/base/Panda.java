a9207828e1a79c2cc2430d52e4cb1ce74e39e710
liangzhichao <472332557@qq.com>
2025-03-25 11:19:21 +0800

    thread join

--- src/main/java/com/liangzc/study25/thread/base/Panda.java
+++ src/main/java/com/liangzc/study25/thread/base/Panda.java
@@ -1,5 +1,7 @@
 package com.liangzc.study25.thread.base;
 
+import org.junit.Test;
+
 /**
  * @Auther: liangzc
  * @Date: 2025/3/25 10:18
@@ -15,15 +17,5 @@ public class Panda implements Runnable{
             throw new RuntimeException(e);
         }
         System.out.println("我叫 花花 呀！");
-    }
-}
-
-
-class RunTest{
-
-    public static void main(String[] args) {
-        new Thread(new Panda()).start();
-        //这就是多线程异步的特征，虽然睡眠1s，但不阻塞主线程的执行
-        System.out.println("主线程执行！");
     }
 }
---
+++ src/main/java/com/liangzc/study25/thread/base/RunTest.java
@@ -0,0 +1,30 @@
+package com.liangzc.study25.thread.base;
+
+import org.junit.Test;
+
+/**
+ * @Auther: liangzc
+ * @Date: 2025/3/25 11:17
+ * @Description:
+ */
+public class RunTest {
+    public static void main(String[] args) {
+        new Thread(new Panda()).start();
+        //这就是多线程异步的特征，虽然睡眠1s，但不阻塞主线程的执行
+        System.out.println("主线程执行！");
+    }
+
+    @Test
+    public void joinTest(){
+        Thread thread = new Thread(new Panda());
+        thread.start();
+        try {
+            //阻塞主线程，直到thread线程执行完毕
+            //thread线程的执行结果对主线程可见，也就是阻塞当前主线程，等待thread线程执行完毕
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("主线程执行！");
+    }
+}
