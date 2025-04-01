63ca398a90a569e3d605121713446ed3028fafc8
liangzhichao <472332557@qq.com>
2025-04-01 16:12:36 +0800

    多线程环境下的原子性

--- src/main/java/com/liangzc/study25/thread/base/SyncExample.java
+++ src/main/java/com/liangzc/study25/thread/base/SyncExample.java
@@ -3,12 +3,12 @@ package com.liangzc.study25.thread.base;
 /**
  * @Auther: liangzc
  * @Date: 2025/3/28 10:43
- * @Description:
+ * @Description: 原子性问题：多线程环境中，多个线程同时访问和修改共享变量，由于cpu时间片的切换，导致最终的结果不确定
  */
 public class SyncExample{
 
     public int count;
-    // 同步方法，增加sync关键字，同一对象，互斥，同一时刻只能有一个线程执行该方法，知道该线程执行完，其他线程才能执行该方法
+    // 同步方法，增加synchronized关键字，同一对象，互斥，同一时刻只能有一个线程执行该方法，直到该线程执行完，其他线程才能执行该方法
     public synchronized void incre(){
         count ++;
     }
