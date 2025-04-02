ecacaeed30b2073af9fd9bb88b3f6cde809765de
liangzhichao <472332557@qq.com>
2025-04-02 11:11:43 +0800

    锁范围测试

--- src/main/java/com/liangzc/study25/thread/base/ThreadStatusExample.java
+++ src/main/java/com/liangzc/study25/thread/base/ThreadStatusExample.java
@@ -55,11 +55,22 @@ public class ThreadStatusExample {
     }
 
     static class BlockDemo implements Runnable{
-
+        /**
+         * LOCK_OBJECT 被声明为 static final transient Object LOCK_OBJECT = new Object();。
+         * static 关键字表示该对象属于类本身，而不是某个具体的实例。因此，所有 BlockDemo 类的实例共享同一个 LOCK_OBJECT 对象。
+         * 当多个线程通过 synchronized (LOCK_OBJECT) 加锁时，实际上是对同一个对象加锁
+         *
+         */
+        static final transient Object LOCK_OBJECT = new Object();
         @Override
         public void run() {
-            //锁的对象是当前类，所用范围是这个类，所以就算是不同对象实例，也只有一个线程能够执行，另一个被阻塞
-            synchronized (BlockDemo.class){
+            /**
+             * 在 run 方法中，使用了 synchronized (LOCK_OBJECT)，而 LOCK_OBJECT 是类级别的静态对象。
+             * 当第一个线程进入 synchronized (LOCK_OBJECT) 块时，它会获取 LOCK_OBJECT 的锁。
+             * 第二个线程尝试进入同一个 synchronized (LOCK_OBJECT) 块时，由于锁已经被第一个线程持有，它必须等待，直到第一个线程释放锁。
+             * 因此，即使两个线程是基于不同的 BlockDemo 实例启动的，它们仍然会因为竞争同一个类级别的锁而发生阻塞。
+             */
+            synchronized (LOCK_OBJECT){
                 while (true){
                     try {
                         TimeUnit.SECONDS.sleep(100);
